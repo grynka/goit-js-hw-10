655e3007fe5c84542f5ad2957d6f2e21848b9bcb
grynka <ihor.kozhemyakin@gmail.com>
2022-10-03 00:57:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
+function fetchCountries(name) {
+    fetch("https://restcountries.com/v3.1/name/{name}")
+        .then(response => {
+            // Response handling
+        })
+        .then(data => {
+            // Data handling
+        })
+        .catch(error => {
+            // Error handling
+        });
+}
 const DEBOUNCE_DELAY = 300;
