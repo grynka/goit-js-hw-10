e8bc57ca6ff619846e928cedf08f1614f7a5c2a0
grynka <ihor.kozhemyakin@gmail.com>
2022-10-06 15:04:10 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import './css/styles.css';
 
+const searchBox = document.querySelector('#search-box');
+console.log(searchBox)
+
+searchBox.addEventListener("input", (event) => {
+    console.log(searchBox.value)
+    fetchCountries(searchBox.value)
+}
+)
+
+
 function fetchCountries(name) {
-    fetch("https://restcountries.com/v3.1/name/{name}")
+    fetch(`https://restcountries.com/v2/all?fields=${name}.official,capital,population,flags.svg,languages`)
         .then(response => {
-            // Response handling
+           console.log(response) // Response handling
         })
         .then(data => {
             // Data handling
