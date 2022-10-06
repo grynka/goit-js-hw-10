a5863ee42cc33fd8e48abfd2e5f44a7c20107eff
grynka <ihor.kozhemyakin@gmail.com>
2022-10-06 20:06:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const searchBox = document.querySelector('#search-box');
-console.log(searchBox)
+const country = document.querySelector('.country-info');
+
 
 searchBox.addEventListener("input", (event) => {
     console.log(searchBox.value)
@@ -11,11 +13,27 @@ searchBox.addEventListener("input", (event) => {
 
 
 function fetchCountries(name) {
-    fetch(`https://restcountries.com/v2/all?fields=${name}.official,capital,population,flags.svg,languages`)
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
-           console.log(response) // Response handling
+         return response.json() // Response handling
         })
         .then(data => {
+            if (data.length === 1) {
+            console.log(data)
+               country.innerHTML = `<li><img src="${data[0].flags.svg}" width="30"> ${data[0].name.official}</li>
+               <li><p class="label">Capital: <p>${data[0].capital}</li>
+               <li><p class="label">Population: <p>${data[0].population}</li>
+               <li><p class="label">Languages: <p>${data[0].languages.key}</li>`
+               
+                }
+
+        else if (data.length <= 10) {
+            console.log(...data)
+            }
+        else if (data.length > 10) {
+            throw Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        }
+
             // Data handling
         })
         .catch(error => {
