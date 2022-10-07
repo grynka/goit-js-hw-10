652ccda4efc43033cdcb4ab04c6e36e4a7de3b11
grynka <ihor.kozhemyakin@gmail.com>
2022-10-07 20:15:32 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
+const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('#search-box');
 const country = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list')
 
 
-searchBox.addEventListener("input", (event) => {
-    console.log(searchBox.value)
-    fetchCountries(searchBox.value)
-}
+searchBox.addEventListener("input", debounce(
+    fetchCountries, DEBOUNCE_DELAY)
 )
 
-
-function fetchCountries(name) {
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+function fetchCountries() {
+    fetch(`https://restcountries.com/v3.1/name/${searchBox.value.trim()}?fields=name,capital,population,flags,languages`)
         .then(response => {
-         return response.json() // Response handling
+            if (!response.ok && searchBox.value.length > 0) {
+                throw new Error( Notiflix.Notify.failure("Oops, there is no country with that name"));
+              }
+              return response.json();
+          // Response handling
         })
         .then(data => {
             if (data.length === 1) {
-            console.log(data)
-               country.innerHTML = `<li><img src="${data[0].flags.svg}" width="30"> ${data[0].name.official}</li>
-               <li><p class="label">Capital: <p>${data[0].capital}</li>
-               <li><p class="label">Population: <p>${data[0].population}</li>
-               <li><p class="label">Languages: <p>${data[0].languages.key}</li>`
-               
+               country.innerHTML = `<p><img src="${data[0].flags.svg}" width="20"> <b>${data[0].name.official}</b></p>
+               <p class="label"><b>Capital: </b>${data[0].capital}</p>
+               <p class="label"><b>Population:  </b>${data[0].population}</p>
+               <p class="label"><b>Languages: </b>${Object.values(data[0].languages)}</p>`
+               countryList.innerHTML = "";
                 }
 
         else if (data.length <= 10) {
-            console.log(...data)
+            countryList.innerHTML = data.map(
+                ({ flags, name }) =>
+                `<li><img src="${flags.svg}" width="25"> <b>${name.official}</b></li>`)
+              .join('')
+              country.innerHTML = "";
             }
         else if (data.length > 10) {
+            countryList.innerHTML = "";
             throw Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
         }
-
-            // Data handling
+        
         })
         .catch(error => {
-            // Error handling
+                     // Error handling
         });
 }
-const DEBOUNCE_DELAY = 300;
